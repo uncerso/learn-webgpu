5f3d414f844c4504dc46d629d1dae2d64dce593b
uncerso <yayayayaya38@gmail.com>
2025-01-07 05:36:29 +0300

    Added more uniform buffers

--- src/tasks/uniform_buffers.cpp
+++ src/tasks/uniform_buffers.cpp
@@ -126,5 +126,91 @@ struct : TaskRegistry {
 
 } // namespace task1
 
+namespace task2 {
+
+const char* shaderSource = R"(
+struct VertexInput {
+    @location(0) pos: vec2f,
+    @location(1) color: vec3f,
+};
+
+struct FragmentInput {
+    @location(0) color: vec3f,
+};
+
+struct VertexOutput {
+    @builtin(position) position: vec4f,
+    @location(0) color: vec3f,
+}
+
+struct UniformData {
+    color: vec4f,
+    time: f32,
+};
+
+@group(0) @binding(0) var<uniform> uData: UniformData;
+
+@vertex
+fn vs_main(in: VertexInput) -> VertexOutput {
+    var out: VertexOutput;
+    let ratio = 640.0 / 480.0;
+
+    var offset = vec2f(-0.6875, -0.463);
+    offset += 0.3 * vec2f(cos(uData.time), sin(uData.time));
+
+    out.position = vec4f(in.pos.x + offset.x, (in.pos.y + offset.y) * ratio, 0.0, 1.0);
+    out.color = in.color;
+    return out;
+}
+
+@fragment
+fn fs_main(in: FragmentInput) -> @location(0) vec4f {
+    return vec4f(in.color, 1.0) * uData.color;
+}
+)";
+
+struct UniformData {
+    glm::vec4 color;
+    float time;
+};
+
+constexpr std::initializer_list<runtime::graphics_engine::Layout const> layouts {{
+    .bindingPoint = task1::UBO_BINDING_POINT,
+    .access = runtime::graphics_engine::Layout::Access::VertexAndFragment,
+}};
+
+struct Renderer : public core::Renderer {
+    static std::unique_ptr<core::Renderer> create(runtime::graphics_engine::IRenderer& renderer) {
+        return std::make_unique<Renderer>(renderer.resourceManager());
+    }
+
+    Renderer(runtime::graphics_engine::IResourceManager& manager)
+        : pipeline(manager.createRenderPipeline({}, shaderSource, task1::attributes, layouts))
+        , mesh(manager.createMesh(std::span{task1::indexData}, std::span{task1::vertexData}))
+        , ubo(manager.createUniformBuffer(UniformData{}))
+    {}
+
+    void render(FrameContext const& context) override {
+        rewrite(*ubo, UniformData{{1.0f, 0.5f, 0.0f, 1.0f}, context.renderTime});
+        auto& encoder = context.renderer.renderPassEncoder();
+        encoder.bindRenderPipeline(*pipeline);
+        encoder.bind(*ubo, task1::UBO_BINDING_POINT);
+        encoder.draw(*mesh);
+    }
+
+    glm::vec4 bgColor() const noexcept override { return {0.05, 0.05, 0.05, 1.0}; }
+
+    std::unique_ptr<runtime::graphics_engine::IRenderPipeline> pipeline;
+    std::unique_ptr<runtime::graphics_engine::IMesh> mesh;
+    std::shared_ptr<runtime::graphics_engine::ITUniformBuffer<UniformData>> ubo;
+};
+
+struct : TaskRegistry {
+    std::string_view name() const noexcept override { return "008 Uniform buffer v+f"; }
+    RendererCreator creator() const noexcept override { return Renderer::create; }
+} _;
+
+} // namespace task2
+
 } // namespace
 } // namespace tasks
